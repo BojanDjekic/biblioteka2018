5acfe61b603f7e90c199f87a708f11b0bbdea872
BojanDjekic <djekic7316@fon.bg.ac.rs>
2018-03-02 15:08:24 +0100

    Uvedena klasa Autor, refaktorisana klasa Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,9 +2,16 @@ package biblioteka;
 public class Knjiga {
 	private String naslov;
 	private String isbn;
-	private String autori;
 	private String izdavac;
 	private int izdanje;
+	private Autor[] autori;
+	
+	public Autor[] getAutori() {
+		return autori;
+	}
+	public void setAutori(Autor[] autori) {
+		this.autori = autori;
+	}
 	public String getNaslov() {
 		return naslov;
 	}
@@ -16,12 +23,6 @@ public class Knjiga {
 	}
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
-	}
-	public String getAutori() {
-		return autori;
-	}
-	public void setAutori(String autori) {
-		this.autori = autori;
 	}
 	public String getIzdavac() {
 		return izdavac;
